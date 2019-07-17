562147f11e06d9602a2d09f84bedbc4ef7fa5a90
Marwan Salem <marwansalem@Marwans-MacBook-Pro.local>
2019-07-17 16:00:57 +0200

    "customAuth Done"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 
 Rails.application.routes.draw do
+
+
+
+  get 'sessions/new'
   get 'welcome/index'
+  get 'signup', to: 'users#new', as: 'signup'
+  # get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :articles do
   resources :comments
 end
+  resources :sessions
+  resources :users
+
+
+
+
 
   root 'welcome#index'
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_124910) do
+ActiveRecord::Schema.define(version: 2019_07_17_134957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2019_07_16_124910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "articles"
